c0cde3e506cfa8329895af500b852db9bce09f9d
Tom Benbow <tom@hellomoon.net>
2022-08-30 13:23:45 -0500

    Layout fix for long schedule

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -30,6 +30,16 @@ module.exports = {
   theme: {
     extend: {
       colors: colorExtend,
+      gridColumnStart: {
+        '13': '13',
+        '14': '14',
+        '15': '15',
+        '16': '16',
+        '17': '17',
+        '18': '18',
+        '19': '19',
+        '20': '20',
+      }
     }
   },
   plugins: [
