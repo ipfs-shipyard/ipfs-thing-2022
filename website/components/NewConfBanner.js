f123d7c31c7ad424a5971b60102533d59c01a78c
b5 <sparkle_pony_2000@qri.io>
2023-04-04 10:06:21 -0400

    add 2023 banner

---
+++ website/components/NewConfBanner.js
@@ -0,0 +1,12 @@
+
+export default function NewConfBanner() {
+  return (
+<div className="overflow-hidden w-full bg-sky-900 p-3">
+  <a href="https://2023.ipfs-thing.io" className="cursor-pointer">
+    <div className="text-center text-xl font-bold max-w-8xl mx-auto text-white">
+    IPFS þing 2023 is happening! Check it out
+    </div>
+  </a>
+</div>
+  )
+}
